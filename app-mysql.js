7954dbcdd4c0ca6af0d548b3fab055b7f50e4a72
BotBalrog <48597449+BotBalrog@users.noreply.github.com>
2019-04-03 19:34:20 +0200

    03/04/2019 19h30
    je suis sur le 2ème chemin actuellement

--- app-mysql.js
+++ app-mysql.js
@@ -6,7 +6,7 @@ const express = require('express'), // Modele de routing - Permet la gestion les
     bdd = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'root',
         database: 'zoubidasql'
     }); // Initialisation de la base de donnée
 const fs = require('fs')
@@ -21,14 +21,62 @@ const path = __dirname + "/index.html"
 
 
 /** START Création de route sous express NodeJs **/
+
+//Route par défaut
 app.get('/', function(req, res) { // Création d'un route sous express
-if(fs.existsSync(path)){
-    res.writeHead(200, { "content-type": "text/plain;" })
-    res.sendFile(__dirname + "/index.html"); // Afficher la page index.html cote client
-}else{
-    res.writeHead(404, { "content-type": "text/plain;" })
-    res.end("Page not found" )
-}})
+    if(fs.existsSync(path)){
+        //res.writeHead(200, { "content-type": "text/plain;" })
+        res.sendFile(__dirname + "/index.html"); // Afficher la page index.html cote client
+    }else{
+        res.writeHead(404, { "content-type": "text/plain;" })
+        res.end("Page not found" )
+    }
+})
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Route pour le Login
+app.post('/login', urlencodedParser, function(req, res) {
+
+    let retour = {
+        error: false,
+        message: []
+    }
+
+    if (req.body.mail !== undefined || req.body.mail.trim() != "" && req.body.password !== undefined || req.body.password.trim() != "") {
+        bdd.query("SELECT * as exist FROM users WHERE email like '" + req.body.email + "' AND password like '" + req.body.password + "';", function(error, result) { // Lancement de la requet SQL
+            if (error)
+                throw error;
+            if(result.lenght == 0){
+                retour.error = true;
+                retour.message.push("votre email ou password est erroné")
+                res.writeHead(401, { "content-type": "application/json; charset=utf-8" })
+                res.end(JSON.stringify(retour))
+            }else{
+                retour.error = false;
+                retour.message.push("L'utilisateur a été authentifié succès")
+                res.writeHead(201, { "content-type": "application/json; charset=utf-8" })
+                res.end(JSON.stringify(result))
+            }
+        })
+    }
+
+    if (req.body.mail === undefined || req.body.mail.trim() == "" || req.body.password === undefined || req.body.password.trim() == "") {
+        retour.error = true;
+        retour.message.push("l'email/password est manquant")
+    }
+
+    if (retour.error == true && (retour.message == "l'email/password est manquant" || retour.message == "Votre email ou password est erroné")) {
+        res.writeHead(401, { "content-type": "application/json; charset=utf-8" })
+        res.end(JSON.stringify(retour))
+    }
+
+    if (retour.error == true && retour.message == "Trop de tentative sur l'email XXXXX - Veuillez patientez 1h") {
+        res.writeHead(409, { "content-type": "application/json; charset=utf-8" })
+        res.end(JSON.stringify(retour))
+    }
+
+})
+////////////////////////////////////////////////////////////////////////////////////////
 
 /** END Création de route sous express NodeJs **/
 
